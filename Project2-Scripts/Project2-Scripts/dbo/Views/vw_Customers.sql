e98e36b3257258f323aa3839ffb892fda9995cd6
Zachary Anderson <zachary.anderson@revature.net>
2021-02-10 09:15:48 -0600

    Added views for Customers and Drivers

---
+++ Project2-Scripts/Project2-Scripts/dbo/Views/vw_Customers.sql
@@ -0,0 +1,6 @@
+﻿CREATE VIEW vw_Customers AS
+	SELECT Customer_ID AS [Customer ID], 
+		Customer_Name AS [Name],
+		Customer_Age AS [Age],
+		Customer_Sex AS [Gender]
+	FROM Customers
---
+++ Project2-Scripts/Project2-Scripts/dbo/Views/vw_Drivers.sql
@@ -0,0 +1,11 @@
+﻿CREATE VIEW vw_Drivers AS
+	SELECT D.Driver_ID AS [Driver ID],
+		D.License_No AS [Liscense Number],
+		D.Driver_Name AS [Name],
+		D.Driver_Age AS [Age],
+		D.Driver_Sex AS [Gender],
+		CONCAT(R.Make, ' ', R.Model) AS [Registered Car],
+		S.State_Name AS [State]
+	FROM States AS S 
+		JOIN Drivers AS D ON S.State_ID = D.State_ID 
+		JOIN Registered_Cars AS R ON R.Registered_Car_ID = D.Registered_Car_ID
